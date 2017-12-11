46b572d27054640a804465652e89fcdae953f067
Chaos Eternal <chaos@shlug.org>
2017-12-11 16:20:02 +0800

    init

---
+++ ods_adapter.py
@@ -0,0 +1,96 @@
+from bosh_api import *
+import yaml, json
+from jsonpath_ng import jsonpath, parse
+
+class OdsAdapter():
+    _env = None
+    _manifest = None
+    _name = None
+    _job = None
+    def __init__(self, name, env, job, must_alivejob = [], manifest=None, render_rules=[]):
+        if not isinstance(env, BoshEnv):
+            raise TypeError("%s.__init__(): env should be instance of BoshEnv"%self.__class__.__name__)
+        self._name = name
+        self._job = job
+        self._render_rules = render_rules
+        self._must_alivejob = must_alivejob
+        self._def_workflow()
+        if manifest is not None:
+            self._manifest = yaml.load(manifest)
+    def __repr__(self):
+        return "<%s %s>"%(self.__class__.__name__, self._name)
+
+    def get_creds(self):
+        cred = {"credentials":
+                {"cluster":
+                 [{"host": i.ips, "port": 8080}
+                  for i in self._env.instances(self._name)
+                     if i.job == self._job]
+                }
+        }
+        return cred
+    def _render_manifest(self):
+        init = self._manifest
+        for p, v in self._render_rules:
+            init = parse(p).update(init, v)
+        return json.dumps(init)
+    def calldeploy(self, task_id = None):
+        if task_id is not None:
+            r = self.checktask(task_id)
+            if r == 'done':
+                return 'deploy_%s'%r, None
+            return "deploy_%s"%r, task_id
+        if self._manifest is None:
+            raise TypeError("%s.calldeploy: manifest is not specified"%self.__class__.__name__)
+        t = self._env.deploy(json.dumps(self._render_manifest()))
+        return self.calldeploy(t.id)
+    def checktask(self, task_id):
+        jobstatemap={"queued":"poolagain",
+                      'processing':"poolagain",
+                      'cancelling':'poolagain',
+                      'error': 'error'}
+        t = self._env.task_by_id(task_id)
+        return jobstatemap.get(t.state, 'done')
+    def callinstancestates(self, task_id):
+        if task_id is not None:
+            r = self.checktask(task_id)
+            return "states_%s"%r, task_id
+        t = self._env.instance_states(self._name)
+        return self.callinstancestates(t.id)
+    def checkstate(self, task_id):
+        t = self._env.task_by_id(task_id)
+        t.set_result_class(BoshInstanceState)
+        res = t.result()
+        if all(i.job_state == 'running' for i in res if i.job_name in self._must_alivejob):
+            return 'deploy_finish',task_id
+        else:
+            return 'deploy_error', task_id
+    def calldelete(self, task_id):
+        if task_id is not None:
+            r = self.checktask(task_id)
+            if r == 'done':
+                return 'delete_done', None
+            return 'delete_%s'%r, task_id
+        t = self._env.delete_deploy(self._name)
+        return self.calldelete(t.id)
+    def _def_workflow(self):
+        self._wf_def = {"deploy": self.calldeploy,
+                        "deploy_poolagain": self.calldeploy,
+                        "deploy_done": self.callinstancestates,
+                        "states_poolagain": self.callinstancestates,
+                        "states_done": self.checkstate,
+                        "deploy_finish": "finish",
+                        "delete": self.calldelete,
+                        "delete_poolagain": self.calldelete,
+                        "delete_done": "finish",
+                        "finish": "finish"
+        }
+    def _workflow(self, pre, t):
+        if pre not in self._wf_def:
+            return 'error', None
+        f = self._wf_def[pre]
+        if callable(f):
+            return f(t)
+        return f, None
+    def workflow(self, pre, t):
+        self._workflow(pre, t)
---
+++ test.py
@@ -0,0 +1,98 @@
+import time
+import ods_adapter
+from bosh_api import *
+
+d = """---
+name: learn-bosh-2
+
+releases:
+- name: learn-bosh
+  version: "0+dev.5"
+- name: syslog
+  version: 11
+
+update:
+  canaries: 1
+  max_in_flight: 2
+  canary_watch_time: 1000-30000
+  update_watch_time: 1000-30000
+
+instance_groups:
+- name: app
+  azs:
+  - z1
+  instances: 1
+  vm_type: minimal
+  stemcell: default
+  update:
+    max_in_flight: 1
+    serial: true
+  networks:
+  - name: default
+  jobs:
+  - name: app
+    release: learn-bosh
+    properties:
+      port: 8888
+      password: 123444
+  - name: router
+    release: learn-bosh
+    properties:
+      port: 8080
+      servers: ["http://10.244.0.20:8888"]
+  - name: syslog_forwarder
+    release: syslog
+    consumes:
+      syslog_storer: {from: primary_syslog_storer}
+- name: syslog_storer_primary
+  azs:
+  - z1
+  jobs:
+  - name: syslog_storer
+    release: syslog
+    provides:
+      syslog_storer: {as: primary_syslog_storer}
+  instances: 1
+  vm_type: minimal
+  persisitent_disk_type: 1GB
+  stemcell: default
+  networks:
+  - name: default
+  properties:
+    syslog:
+      transport: tcp
+stemcells:
+- alias: default
+  os: ubuntu-trusty
+  version: "3468.5"
+"""
+def wait(t):
+    while t.state != 'done':
+        time.sleep(1)
+        print(".", end="", flush=True)
+        t.update()
+    print("")
+
+def main():
+    dname = "learn-bosh-2"
+    env=BoshEnv("192.168.50.6", os.getenv("BOSH_CLIENT"), os.getenv("BOSH_CLIENT_SECRET"),
+                cacert=os.getenv("BOSH_CA_CERT"))
+    adp = ods_adapter.OdsAdapter("learn-bosh-2", env, "app", ["app"], d)
+
+    n, t = adp.workflow("delete", None)
+    while True:
+        print n, t
+        if n == "finish" or n == "error":
+            break
+        n, t = adp.workflow(n, t)
+        time.sleep(30)
+
+    n, t = adp.workflow("deploy", None)
+    while True:
+        print n, t
+        if n == "finish" or n == "error":
+            break
+        n, t = adp.workflow(n, t)
+        time.sleep(30)
+if __name__ == '__main__':
+    main()
